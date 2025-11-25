e203a6509069efe84121a17ef95b3df36ef512d2
GRDavies75 <gdavies@home.nl>
2025-11-25 14:41:24 +0100

    extra examples of skipping

--- section_3/py_assertions/test_skiptests.py
+++ section_3/py_assertions/test_skiptests.py
@@ -6,8 +6,8 @@ import pytest
 FAHRENHEIT_TO_CELSIUS_RATIO = 9/5
 FAHRENHEIT_TO_CELSIUS_OFFSET = 32
 
-def celsius_to_fahrenheit(celc: float = 0.0) -> float:
-    fahrenheit = (celc * FAHRENHEIT_TO_CELSIUS_RATIO) + FAHRENHEIT_TO_CELSIUS_OFFSET
+def celsius_to_fahrenheit(celcius: float = 0.0) -> float:
+    fahrenheit = (celcius * FAHRENHEIT_TO_CELSIUS_RATIO) + FAHRENHEIT_TO_CELSIUS_OFFSET
     return fahrenheit
 
 
@@ -19,10 +19,12 @@ def test_case01() -> None:
     assert type(FAHRENHEIT_TO_CELSIUS_RATIO) == float
 
 
-@pytest.mark.skipif(sys.version_info > (3,8), .reason="Does not work on python version above 3.8")
+# @pytest.mark.skipif(sys.version_info > (3,8), reason="Does not work on python version above 3.8")
+@pytest.mark.skipif(celsius_to_fahrenheit() == 32, reason="Default value test, so skipping")
 def test_case02() -> None:
     assert celsius_to_fahrenheit() == 32
 
 
+@pytest.mark.skipif(pytest.__version__ > '5.5.0', reason="pytest version is greater then 5.5.0")
 def test_case03() -> None:
     assert celsius_to_fahrenheit(38) == 100.4
