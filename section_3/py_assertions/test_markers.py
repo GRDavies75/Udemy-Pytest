05e1e8c7555a09d4320c4f6f0539cf4ff3f90df0
GRDavies75 <gdavies@home.nl>
2025-11-25 15:50:51 +0100

    grouping with markers example

---
+++ section_3/py_assertions/test_markers.py
@@ -0,0 +1,32 @@
+import pytest   
+pytestmark = [pytest.mark.smoke, pytest.mark.sanity, pytest.mark.str]
+
+@pytest.mark.sanity
+def test_str01() -> None:
+    num = 9/4
+    s1 = 'I like ' + 'pytest automation'
+    assert str(num) == '2.25'
+    assert s1 == 'I like pytest automation'
+    assert s1 + str(num) == 'I like pytest automation2.25'
+
+
+@pytest.mark.sanity
+def test_str02() -> None:
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    assert len(letters) == 26
+
+
+@pytest.mark.sanity
+@pytest.mark.str
+def test_str03() -> None:
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    assert letters[0] == 'a'
+    assert letters[-1] == 'z' == letters[25]
+
+
+def test_strslice() -> None:
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    assert letters[:] == letters
+    assert letters[10:] == 'klmnopqrstuvwxyz'
+    assert letters[-3:] == 'xyz'
+    assert letters[:21:5] == 'afkpu'
