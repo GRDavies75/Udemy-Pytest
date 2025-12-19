78e50b638442ec829ed9098dc00ef7e705d42c2c
GRDavies75 <gdavies@home.nl>
2025-12-19 13:45:22 +0100

    more bdd parameterized

--- bddpytest/myfeatures/parameterize.feature
+++ bddpytest/myfeatures/parameterize.feature
@@ -7,3 +7,11 @@ Feature: Parameterizing tests in Pytest BDD
         Then There is same count of varieties
         But If we add a different variety of fruit
         Then The count of varieties increases to 4
+
+
+    Scenario: Parameterize benefits
+        Given Given there are 5 fruits
+        When I eat 3 fruits
+        And I eat 2 fruits
+        Then I should have 0 fruits
+
--- bddpytest/test_bddparameterize.py
+++ bddpytest/test_bddparameterize.py
@@ -21,20 +21,44 @@ def existing_fruits() -> set[str]:
 
 
 @when('We add a same variety of fruit')
-def add_a_fruit(fruits) -> None:
+def add_a_fruit(fruits: set[str]) -> None:
     fruits.add("grapes")
 
 
 @then('There is same count of varieties')
-def same_count(fruits) -> None:
+def same_count(fruits: set[str]) -> None:
     assert len(fruits) == 3
 
 
 @then('If we add a different variety of fruit')
-def add_diff_variety(fruits) -> None:
+def add_diff_variety(fruits: set[str]) -> None:
     fruits.add("cherry")
 
 
 @then(parsers.parse('The count of varieties increases to {count:d}'))
-def count_increases(fruits, count) -> None:
+def count_increases(fruits: set[str], count: int) -> None:
     assert len(fruits) == count
+
+# ----------------- End of scenario 1 ------------------
+
+# Global variable
+pytest.total_fruits = 0 # type: ignore
+
+
+@given(parsers.parse('Given there are {count:d} fruits'), target_fixture='start_fruits')
+def existing_dict_of_fruits(count: int) -> dict[str, int]:
+    pytest.total_fruits = count # type: ignore
+
+    return dict(start=count, eat=0)
+
+
+@when(parsers.parse('I eat {eat:d} fruits'))
+def eat_fruits(start_fruits, eat) -> None:
+    start_fruits["eat"] += eat
+    print(start_fruits)
+
+
+@then(parsers.parse('I should have {left:d} fruits'))
+def should_have_left_fruits(start_fruits: dict[str, int], left):
+    assert start_fruits['start'] == pytest.total_fruits # type: ignore
+    assert start_fruits['start'] - start_fruits['eat'] == left
